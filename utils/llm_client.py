787782ab5d0bc7a8755533edb0e50b57efaa76ea
arctouch-marianasantana <mariana.santana@arctouch.com>
2025-09-29 20:37:31 -0300

    atualiza para nao depender de env local

--- utils/llm_client.py
+++ utils/llm_client.py
@@ -1,8 +1,9 @@
-import os
+import streamlit as st
+
 from dotenv import load_dotenv
 from openai import OpenAI
 
 load_dotenv()  # Carrega variáveis do .env
 
 def get_client() -> OpenAI:
-    return OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+    return OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
